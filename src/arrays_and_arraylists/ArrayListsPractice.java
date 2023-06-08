414aeb28dfd0f56c3e9b6b3ae35c177d0c443ba0
Binod Acharya <acharyabinod0422@gmail.com>
2023-06-08 11:12:01 -0400

    "Finish the activity"

--- src/arrays_and_arraylists/ArrayListsPractice.java
+++ src/arrays_and_arraylists/ArrayListsPractice.java
@@ -1,8 +1,22 @@
 package arrays_and_arraylists;
-
+import java.sql.SQLOutput;
+import java.util.*;
 public class ArrayListsPractice {
     public static void main(String[] args) {
+        ArrayList<String> fruits = new ArrayList<String>();
 //        Create an ArrayList
+            fruits.add("Apple");
+            fruits.add("Orange");
+            fruits.add("Banana");
+            fruits.add("Pineapple");
+            fruits.add("Mango");
+            System.out.println(fruits);
+            System.out.println(fruits.size());
+            fruits.remove("Apple");
+            System.out.println(fruits);
+            fruits.set(1, "Strawberry");
+            System.out.println(fruits);
+
 //        add 5 elements to it
 //        print the entire ArrayList to the terminal
 //        print the length of it
@@ -11,7 +25,12 @@ public class ArrayListsPractice {
 //        change some values in the ArrayList
 //        print it again to see your changes
 
+
+        ArrayList<Integer> num = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
+        num.remove(1);
+        System.out.println(num);
 //        Create an ArrayList with 5 elements and give the value in-line (you will need to use Arrays.asList())
+
 
 //        mess around with a couple methods for ArrayLists. Just type out the ArrayList variable name you made and then put a "." and scroll through the list of methods.
 //        read the information it shows you and try to figure out how to use it.
--- src/arrays_and_arraylists/ArraysPractice.java
+++ src/arrays_and_arraylists/ArraysPractice.java
@@ -1,18 +1,23 @@
 package arrays_and_arraylists;
-
+import java.util.Arrays;
 public class ArraysPractice {
     public static void main(String[] args) {
-//        Create an empty array with 5 elements
-//        give each index in the array a value
-//        print the entire array to the terminal (you will need to use the Array.toString() method)
-//        print the length of the array
+        String[] fruits = new String[5];
+        fruits[0] = "apple";
+        fruits[1] = "banana";
+        fruits[2] = "mango";
+        fruits[3] = "avacado";
+        fruits[4] = "orange";
+        System.out.println(Arrays.toString(fruits));
+        System.out.println(fruits.length);
 
-//        Create an array with 5 elements and give the values in-line
-//        print the entire array to the terminal
-//        print the length of the array
+        String[] operatingSystem = {"Windows XP" ,"Windows 7", "Windows 8", "Windows 10", "Window 11"};
+        System.out.println(Arrays.toString(operatingSystem));
 
 //        change a few values in each array and then print both arrays to the terminal again
-        System.out.println("Hello World");
+        System.out.println(operatingSystem.length);
+        operatingSystem[2] = "Windows 8 Home";
+        System.out.println(Arrays.toString(operatingSystem));
     }
 
 
